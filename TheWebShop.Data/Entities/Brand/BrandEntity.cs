3da60e426512c7dd5ae947aa1e536464b9beccbf
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 21:02:20 +0200

    Added brand entity

---
+++ TheWebShop.Data/Entities/Brand/BrandEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TheWebShop.Data.Entities.Product;
+
+namespace TheWebShop.Data.Entities.Brand
+{
+    public class BrandEntity : BaseEntity, IBrandEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<ProductEntity> Products { get; set; }
+    }
+}
