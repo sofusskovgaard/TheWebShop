cc3f8ebe0218380f1cd6e70a473a1fc5d070915b
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 18:15:16 +0200

    Created database context

---
+++ TheWebShop.Data/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TheWebShop.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TheWebShop;Trusted_Connection=True;")
+            .EnableSensitiveDataLogging(true)
+            .UseLoggerFactory(new ServiceCollection()
+            .AddLogging(builder => builder.AddConsole()
+                .AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information))
+                .BuildServiceProvider().GetService<ILoggerFactory>());
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
