6c5bc15b8181754333517c06c6b97a02b1549b45
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 21:06:15 +0200

    Added product category entity configuration

---
+++ TheWebShop.Data/Entities/ProductCategory/ProductCategoryEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TheWebShop.Data.Entities.ProductCategory
+{
+    public class ProductCategoryEntityConfiguration : BaseEntityConfiguration<ProductCategoryEntity>
+    {
+        public override void Configure(EntityTypeBuilder<ProductCategoryEntity> builder)
+        {
+            base.Configure(builder);
+
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Products)
+                .HasForeignKey(x => x.CategoryEntityId);
+
+            builder
+                .HasOne(x => x.Product)
+                .WithMany(x => x.Categories)
+                .HasForeignKey(x => x.ProductEntityId);
+        }
+    }
+}
