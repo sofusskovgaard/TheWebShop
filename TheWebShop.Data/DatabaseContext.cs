0c2a9bbce18f77eeb60d10935815e3dce7932ae7
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 22:21:46 +0200

    Added data seeding and fixed cofiguration applyment

--- TheWebShop.Data/DatabaseContext.cs
+++ TheWebShop.Data/DatabaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -7,15 +8,27 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using TheWebShop.Data.Entities;
+using TheWebShop.Data.Entities.Brand;
+using TheWebShop.Data.Entities.Category;
+using TheWebShop.Data.Entities.Product;
+using TheWebShop.Data.Entities.ProductCategory;
+using TheWebShop.Data.Entities.ProductPicture;
 
 namespace TheWebShop.Data
 {
     public class DatabaseContext : DbContext
     {
+
+        public DbSet<BrandEntity> BrandEntities { get; set; }
+        public DbSet<CategoryEntity> CategoryEntities { get; set; }
+        public DbSet<ProductEntity> ProductEntities { get; set; }
+        public DbSet<ProductCategoryEntity> ProductCategoryEntities { get; set; }
+        public DbSet<ProductPictureEntity> ProductPictureEntities { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TheWebShop;Trusted_Connection=True;")
-            .EnableSensitiveDataLogging(true)
+            optionsBuilder.UseSqlServer("Server=localhost;Database=TheWebShop;User Id=sa;Password=P@ssw0rd;")
+            //.EnableSensitiveDataLogging(true)
             .UseLoggerFactory(new ServiceCollection()
             .AddLogging(builder => builder.AddConsole()
                 .AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Information))
@@ -24,32 +37,122 @@ namespace TheWebShop.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+#if DEBUG
+
+            modelBuilder.SeedDatabase();
+
+#endif
+
             modelBuilder.Ignore<BaseEntity>();
 
-            modelBuilder.ApplyConfigurations();
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(DatabaseContext)));
         }
     }
 
     public static class DatabaseContextExtensions
     {
-        public static ModelBuilder ApplyConfigurations(this ModelBuilder modelBuilder)
+        public static ModelBuilder SeedDatabase(this ModelBuilder modelBuilder)
         {
+            #region Brands
 
-            var types = Assembly
-                .GetExecutingAssembly()
-                .GetTypes()
-                .Where(type => type
-                   .GetInterfaces()
-                   .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<BaseEntity>))
-                ).ToList();
+            modelBuilder.Entity<BrandEntity>()
+                .HasData(
+                    new BrandEntity()
+                    {
+                        EntityId = 1,
+                        Name = "Den gamle dame nede på havnen"
+                    },
+                    new BrandEntity()
+                    {
+                        EntityId = 2,
+                        Name = "Din nabo"
+                    }
+                );
 
-            foreach (var type in types)
-            {
-                dynamic configurationInstance = Activator.CreateInstance(type);
-                modelBuilder.ApplyConfiguration(configurationInstance);
-            }
+            #endregion
+
+            #region Categories
+
+            modelBuilder.Entity<CategoryEntity>()
+                .HasData(
+                    new CategoryEntity()
+                    {
+                        EntityId = 1,
+                        Name = "Cannabis"
+                    },
+                    new CategoryEntity()
+                    {
+                        EntityId = 2,
+                        Name = "Processeret",
+                        ParentCategoryEntityId = 1
+                    },
+                    new CategoryEntity()
+                    {
+                        EntityId = 3,
+                        Name = "Plante",
+                        ParentCategoryEntityId = 1
+                    }
+                );
+
+            #endregion
+
+            #region Products
+
+            modelBuilder.Entity<ProductEntity>()
+                .HasData(
+                    new ProductEntity()
+                    {
+                        EntityId = 1,
+                        Name = "Lemon Haze",
+                        Price = 10.95,
+                        BrandEntityId = 1
+                    },
+                    new ProductEntity()
+                    {
+                        EntityId = 2,
+                        Name = "California Kush",
+                        Price = 15.95,
+                        BrandEntityId = 1
+                    },
+                    new ProductEntity()
+                    {
+                        EntityId = 3,
+                        Name = "Skunk",
+                        Price = 5.95,
+                        BrandEntityId = 2
+                    }
+                );
+
+            #endregion
+
+            #region Product Categories
+
+            modelBuilder.Entity<ProductCategoryEntity>()
+                .HasData(
+                    new ProductCategoryEntity()
+                    {
+                        EntityId = 1,
+                        ProductEntityId = 1,
+                        CategoryEntityId = 1
+                    },
+                    new ProductCategoryEntity()
+                    {
+                        EntityId = 2,
+                        ProductEntityId = 2,
+                        CategoryEntityId = 1
+                    },
+                    new ProductCategoryEntity()
+                    {
+                        EntityId = 3,
+                        ProductEntityId = 3,
+                        CategoryEntityId = 2
+                    }
+                );
+
+            #endregion
 
             return modelBuilder;
-        } 
+        }
     }
 }
