82e4f950d8b16b7cf47bfd9112d2a5e37e35326e
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 18:31:09 +0200

    Added base entity configuration

---
+++ TheWebShop.Data/EntityConfigurations/BaseEntityConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using TheWebShop.Data.Entities;
+
+namespace TheWebShop.Data.EntityConfigurations
+{
+    public class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.Property(x => x.CreatedAt).HasDefaultValueSql("GETDATE()");
+            builder.Property(x => x.CreatedAt).ValueGeneratedOnAdd();
+
+            builder.Property(x => x.UpdatedAt).HasDefaultValueSql("GETDATE()");
+            builder.Property(x => x.UpdatedAt).ValueGeneratedOnAddOrUpdate();
+
+            builder.Property(x => x.RowVersion).IsRowVersion();
+        }
+    }
+}
