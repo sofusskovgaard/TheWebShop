d62c9a987159edef86be1b7297dfea38a623914f
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 21:02:39 +0200

    Added product entity

---
+++ TheWebShop.Data/Entities/Product/IProductEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TheWebShop.Data.Entities.Brand;
+using TheWebShop.Data.Entities.ProductCategory;
+using TheWebShop.Data.Entities.ProductPicture;
+
+namespace TheWebShop.Data.Entities.Product
+{
+    public interface IProductEntity : IBaseEntity
+    {
+        string Name { get; set; }
+
+        int? BrandEntityId { get; set; }
+
+        BrandEntity Brand { get; set; }
+
+        double Price { get; set; }
+
+        ICollection<ProductPictureEntity> Pictures { get; set; }
+
+        ICollection<ProductCategoryEntity> Categories { get; set; }
+    }
+}
