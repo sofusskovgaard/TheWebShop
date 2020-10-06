0ea6b73dd9c5d94761c057ae2f929f02bd3d0fd2
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 21:02:52 +0200

    Added product catgegory entity

---
+++ TheWebShop.Data/Entities/ProductCategory/ProductCategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TheWebShop.Data.Entities.Category;
+using TheWebShop.Data.Entities.Product;
+
+namespace TheWebShop.Data.Entities.ProductCategory
+{
+    public class ProductCategoryEntity : BaseEntity, IProductCategoryEntity
+    {
+        public int? ProductEntityId { get; set; }
+
+        public ProductEntity Product { get; set; }
+
+        public int? CategoryEntityId { get; set; }
+
+        public CategoryEntity Category { get; set; }
+    }
+}
