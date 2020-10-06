c6015682645323deb81d4cbdee215c02484b0e7c
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-06 21:03:06 +0200

    Added product picture entity

---
+++ TheWebShop.Data/Entities/ProductPicture/IProductPictureEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TheWebShop.Data.Entities.Product;
+
+namespace TheWebShop.Data.Entities.ProductPicture
+{
+    public interface IProductPictureEntity : IBaseEntity
+    {
+        int? ProductEntityId { get; set; }
+
+        ProductEntity Product { get; set; }
+
+        string Caption { get; set; }
+
+        byte[] Picture { get; set; }
+    }
+}
