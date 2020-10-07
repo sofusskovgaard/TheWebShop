87dc4a1ee66355307c847c9f0f6904aa13f4452e
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-07 23:46:15 +0200

    Added review data access service and extensions

---
+++ TheWebShop.Services/DataAccessServices/Review/ReviewDataAccessExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+using TheWebShop.Common.Filters.Product;
+using TheWebShop.Common.Filters.Review;
+using TheWebShop.Data.Entities.Product;
+using TheWebShop.Data.Entities.Review;
+
+namespace TheWebShop.Services.DataAccessServices.Review
+{
+    public static class ReviewDataAccessExtensions
+    {
+        public static IQueryable<ReviewEntity> FilterEntities(
+           this IQueryable<ReviewEntity> entities, ReviewFilter filter
+       )
+        {
+            var filteredEntities = entities;
+
+            if (!string.IsNullOrEmpty(filter.Query))
+            {
+                filteredEntities = filteredEntities.Where(x => x.Title.ToLower().Contains(filter.Query.ToLower()));
+            }
+
+            if (filter.Product != null)
+            {
+                filteredEntities = filteredEntities.Where(
+                    x => x.ProductEntityId == filter.Product
+                );
+            }
+
+            if (filter.MinRating != null)
+            {
+                filteredEntities = filteredEntities.Where(
+                    x => x.Rating >= filter.MinRating
+                );
+            }
+
+            if (filter.MaxRating != null)
+            {
+                filteredEntities = filteredEntities.Where(
+                    x => x.Rating <= filter.MaxRating
+                );
+            }
+
+            return filteredEntities;
+        }
+
+        public static IQueryable<ReviewEntity> OrderEntities(
+            this IQueryable<ReviewEntity> entities, ReviewFilter filter
+        )
+        {
+            switch (filter.OrderBy)
+            {
+                case ReviewOrderBy.None:
+                    return entities;
+
+                case ReviewOrderBy.RatingAsc:
+                    return entities.OrderBy(x => x.Rating);
+
+                case ReviewOrderBy.RatingDesc:
+                    return entities.OrderByDescending(x => x.Rating);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(filter.OrderBy), filter.OrderBy, null);
+            }
+        }
+
+        public static IQueryable<ReviewEntity> PaginateEntities(
+            this IQueryable<ReviewEntity> entities, ReviewFilter filter
+        )
+        {
+            return entities.Skip((filter.Page - 1) * filter.PageSize).Take(filter.PageSize);
+        }
+    }
+}
