c4ac84875fc6ea96d99acc8bc0b76647202752cb
Sofus Alexander Skovgaard <sofus.skovgaard@gmail.com>
2020-10-07 23:14:29 +0200

    Added product data access service and extensions

---
+++ TheWebShop.Services/DataAccessServices/Product/ProductDataAccessExtensions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TheWebShop.Common.Filters;
+using TheWebShop.Common.Filters.Product;
+using TheWebShop.Data.Entities.Product;
+
+namespace TheWebShop.Services.DataAccessServices.Product
+{
+    public static class ProductDataAccessExtensions
+    {
+        public static IQueryable<ProductEntity> FilterEntities(
+            this IQueryable<ProductEntity> entities, ProductFilter filter
+        )
+        {
+            var filteredEntities = entities;
+
+            if (!string.IsNullOrEmpty(filter.Query))
+            {
+                filteredEntities = filteredEntities.Where(x => x.Name.ToLower().Contains(filter.Query.ToLower()));
+            }
+                
+            if (filter.Category != null)
+            {
+                filteredEntities =
+                    filteredEntities.Where(
+                        x => x.Categories.Any(
+                            c => c.CategoryEntityId == filter.Category
+                        )
+                    );
+            }
+
+            if (filter.MinPrice != null)
+            {
+                filteredEntities = filteredEntities.Where(x => x.Price >= filter.MinPrice);
+            }
+
+            if (filter.MaxPrice != null)
+            {
+                filteredEntities = filteredEntities.Where(x => x.Price <= filter.MaxPrice);
+            }
+
+            if (filter.MinRating != null)
+            {
+                filteredEntities = filteredEntities.Where(
+                    x => x.Reviews.Sum(r => r.Rating) / x.Reviews.Count >= filter.MinRating
+                );
+            }
+
+            if (filter.MaxRating != null)
+            {
+                filteredEntities = filteredEntities.Where(
+                    x => x.Reviews.Sum(r => r.Rating) / x.Reviews.Count <= filter.MaxRating
+                );
+            }
+
+            return filteredEntities;
+        }
+
+        public static IQueryable<ProductEntity> OrderEntities(
+            this IQueryable<ProductEntity> entities, ProductFilter filter
+        )
+        {
+            switch (filter.OrderBy)
+            {
+                case ProductOrderBy.None:
+                    return entities;
+
+                case ProductOrderBy.NameAsc:
+                    return entities.OrderBy(x => x.Name);
+
+                case ProductOrderBy.NameDesc:
+                    return entities.OrderByDescending(x => x.Name);
+
+                case ProductOrderBy.ReviewsAsc:
+                    return entities.OrderBy(x => x.Reviews.Sum(r => r.Rating) / x.Reviews.Count);
+
+                case ProductOrderBy.ReviewsDesc:
+                    return entities.OrderByDescending(x => x.Reviews.Sum(r => r.Rating) / x.Reviews.Count);
+
+                case ProductOrderBy.PriceAsc:
+                    return entities.OrderBy(x => x.Price);
+
+                case ProductOrderBy.PriceDesc:
+                    return entities.OrderByDescending(x => x.Price);
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(filter.OrderBy), filter.OrderBy, null);
+            }
+        }
+
+        public static IQueryable<ProductEntity> PaginateEntities(
+            this IQueryable<ProductEntity> entities, ProductFilter filter
+        )
+        {
+            return entities.Skip((filter.Page - 1) * filter.PageSize).Take(filter.PageSize);
+        }
+    }
+}
---
+++ TheWebShop.Services/DataAccessServices/Product/ProductDataAccessService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using TheWebShop.Common.Filters.Product;
+using TheWebShop.Data;
+using TheWebShop.Data.Entities.Product;
+
+namespace TheWebShop.Services.DataAccessServices.Product
+{
+    public class ProductDataAccessService : BaseDataAccessService<ProductEntity, ProductFilter, ProductOrderBy>
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductDataAccessService(DatabaseContextFactory databaseContextFactory)
+        {
+            this._context = databaseContextFactory.CreateDbContext(null);
+        }
+
+        public override async Task<ProductEntity> GetById(int entityId)
+        {
+            var product = await _context.Products
+                              .AsNoTracking()
+                              .FirstOrDefaultAsync(x => x.EntityId == entityId);
+
+            return product;
+        }
+
+        public override async Task<IEnumerable<ProductEntity>> GetByFilter(ProductFilter filter)
+        {
+            var products = await _context.Products
+                .AsNoTracking()
+                .FilterEntities(filter)
+                .OrderEntities(filter)
+                .PaginateEntities(filter)
+                .ToListAsync();
+
+            return products;
+        }
+
+        public override async Task<ProductEntity> UpdateById(int entityId, object data)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.EntityId == entityId);
+
+            foreach (var property in product.GetType().GetProperties())
+            {
+                try
+                {
+                    var propertyValue = property.GetValue(product);
+                    var updatedProperty = data.GetType().GetProperty(property.Name);
+                    var updatedPropertyValue = updatedProperty.GetValue(data);
+
+                    if (propertyValue != updatedPropertyValue)
+                    {
+                        property.SetValue(product, updatedPropertyValue);
+                    }
+                }
+                catch (NullReferenceException ex)
+                {
+
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public override async Task<bool> DeleteById(int entityId)
+        {
+            try
+            {
+                var product = await _context.Products.FirstOrDefaultAsync(x => x.EntityId == entityId);
+
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
