82bd84b37132f741d0a4883ce1ab2e22e7a94e68
Sofus Skovgaard <sofus.skovgaard@gmail.com>
2020-10-14 17:01:25 +0200

    * Program.cs: * Startup.cs: * appsettings.json: * site.js: * Error.cshtml: * Index.cshtml: * favicon.ico: * site.css: * Privacy.cshtml: * Index.cshtml.cs: * Error.cshtml.cs: * hero.jpg: * Privacy.cshtml.cs: * _ViewStart.cshtml: * TheWebShop.WebApp.csproj: * _ViewImports.cshtml: * _Layout.cshtml: * appsettings.Development.json: * LICENSE: * launchSettings.json: * LICENSE.txt: * jquery.js: * Hero.cshtml: * Footer.cshtml: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.min.js: * bootstrap.js.map: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * isaac-quesada-Loahp7cXIf4-unsplash.jpg: * jquery.validate.js: * LICENSE.txt: * bootstrap-grid.min.css: * bootstrap.bundle.min.js: * bootstrap.bundle.js.map: * bootstrap-grid.css.map: * bootstrap-reboot.min.css: * bootstrap-reboot.css.map: * additional-methods.js: * bootstrap.bundle.min.js.map: * jquery.validate.min.js: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:
    * TheWebShop.sln: Added web app

---
+++ TheWebShop.WebApp/Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TheWebShop.Data;
+using TheWebShop.Services.CachingServices;
+using TheWebShop.Services.DataAccessServices.Brand;
+using TheWebShop.Services.DataAccessServices.Category;
+using TheWebShop.Services.DataAccessServices.Product;
+using TheWebShop.Services.DataAccessServices.Review;
+
+namespace TheWebShop.WebApp
+{
+public class Startup
+{
+public Startup(IConfiguration configuration)
+{
+Configuration = configuration;
+}
+public IConfiguration Configuration { get; }
+
+// This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddScoped<IDatabaseContextFactory, DatabaseContextFactory>();
+
+            //services.AddDistributedRedisCache(
+            //    options =>
+            //    {
+            //        var host = "localhost";
+            //        var port = 6379;
+
+            //        options.Configuration = $"{host}:{port}";
+            //    }
+            //);
+
+            //services.AddScoped<ICachingService, CachingService>();
+
+            // Configure data access services
+            services.AddScoped<IProductDataAccessService, ProductDataAccessService>();
+            services.AddScoped<ICategoryDataAccessService, CategoryDataAccessService>();
+            services.AddScoped<IBrandDataAccessService, BrandDataAccessService>();
+            services.AddScoped<IReviewDataAccessService, ReviewDataAccessService>();
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
